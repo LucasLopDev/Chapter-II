ec3af49ddfc868c038543b99d4bc645ac486b9fe
LucasLopDev <lopes0022@gmail.com>
2022-10-16 22:52:59 -0300

    Before_Connection

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { createConnection } from "typeorm";
+
+createConnection();
+
+// import { DataSource } from "typeorm";
+
+// const dataSource = new DataSource({
+//   type: "postgres",
+//   host: "database",
+//   port: 5432,
+//   username: "docker",
+//   password: "12345",
+//   database: "avalon",
+// });
+
+// dataSource.initialize();
